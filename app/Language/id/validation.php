b4035c50651fe98eb1673550918ded57f86c1e59
Mosyahizuku <62657189+mosyahi@users.noreply.github.com>
2024-07-15 23:32:35 +0700

    -

--- app/Language/id/validation.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    // ...
-    'password' => [
-        'regex_match' => 'Password harus memiliki setidaknya 8 karakter dengan di sertai huruf kapital, angka, dan tanda unik seperti (.,@$)'
-    ],
-];
